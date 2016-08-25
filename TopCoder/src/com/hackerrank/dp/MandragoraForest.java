bf4ba0323fa6a56cc332c57d1be421b13aae8d43
sumanshil <suman_krec@yahoo.com>
2016-08-24 23:52:10 -0700

    adding more classes

--- TopCoder/src/com/hackerrank/dp/MandragoraForest.java
+++ TopCoder/src/com/hackerrank/dp/MandragoraForest.java
@@ -1,5 +1,7 @@
 package com.hackerrank.dp;
 
+import java.util.Arrays;
+
 /**
  * Created by sshil on 8/17/2016.
  */
@@ -7,6 +9,7 @@ package com.hackerrank.dp;
 public class MandragoraForest {
 
     private static int maxExperience = Integer.MIN_VALUE;
+
 
     public static void calculate(int[] arr){
         recursive(arr.length-1, arr, 1, 0);
@@ -26,8 +29,22 @@ public class MandragoraForest {
     }
 
 
+    public void dp(int[] arr){
+        long total  = Arrays.stream(arr).sum();
+        Arrays.sort(arr);
+        long maxStrength = 0;
+        for ( int i = 0 ; i < arr.length ; i++){
+            maxStrength = Math.max(maxStrength, (i+1)*total);
+            total -= arr[i];
+        }
+        System.out.println(maxStrength);
+
+    }
+
+
     public static void main(String[] args) {
-        int[] arr = {3, 2, 2};
-        new MandragoraForest().calculate(arr);
+        int[] arr = {1188729 ,1052940, 9159214 ,1041177, 1109408, 1127712, 708150, 1358977, 769261, 990618, 8711306 ,656084 ,156949, 1188775 ,9153873 ,9345902};
+        new MandragoraForest().dp(arr);
+
     }
 }
---
+++ TopCoder/src/com/oracle/GenerationRequestSchedule.java
@@ -0,0 +1,82 @@
+package com.oracle;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.HashSet;
+import java.util.Set;
+
+import static com.oracle.FacilityMapping.DSVS;
+
+/**
+ * Created by sshil on 8/24/2016.
+ */
+public class GenerationRequestSchedule {
+
+
+//    public static void main(String[] args) {
+//        for ( int i = 0 ; i < 10; i++ ) {
+//            String str = "INSERT INTO wbr_generation_request_schedule (jobgroup,schedule,status) VALUES ('DSV_OrderReleaseGen_Group"+(i+1)+"','*/1 * * * *','COMPLETE');";
+//            System.out.println(str);
+//        }
+//
+//    }
+
+    //
+    public static void main(String[] args) {
+        try {
+            String[] arr = DSVS.split(",");
+            Set<String> dsvs = new HashSet<>();
+            for (String str : arr) {
+                dsvs.add(str);
+            }
+            String str = String.join(",", dsvs);
+            String dbURL = "jdbc:oracle:thin:@(DESCRIPTION=(ADDRESS_LIST=(ADDRESS=(PROTOCOL=TCP)(HOST=ndc-vedip1.walmart.com)(PORT=1521)))(CONNECT_DATA=(SERVICE_NAME=edip1)))";
+            String strUserID = "integration_user";
+            String strPassword = "intgrt10n1";
+            Connection myConnection = DriverManager.getConnection(dbURL, strUserID, strPassword);
+
+            Statement sqlStatement = myConnection.createStatement();
+            /*
+            String readRecordSQL = "select count(*) from wfu_distributor where distributor_id in (" + str + ")";
+            ResultSet myResultSet = sqlStatement.executeQuery(readRecordSQL);
+            while (myResultSet.next()) {
+                //System.out.println("Record values: " + myResultSet.getString("distributor_id"));
+                String child = myResultSet.getString(1);
+                System.out.println(child);
+            }
+            myResultSet.close();
+            */
+
+
+            String readRecordSQL = "select * from wfu_distributor where distributor_id in (" + str + ")";
+            ResultSet myResultSet = sqlStatement.executeQuery(readRecordSQL);
+            int groupIndex = 1;
+            int count = 0;
+            while (myResultSet.next()) {
+                //System.out.println("Record values: " + myResultSet.getString("distributor_id"));
+                if (count == 85){
+                    count = 0;
+                    groupIndex++;
+                }
+                String distributorId = myResultSet.getString("DISTRIBUTOR_ID");
+                String distributorName = myResultSet.getString("DISTRIBUTOR_NAME");
+
+
+                String str1 = "INSERT INTO wbr_batch_scheduler (jobgroup,jobtype,blocking) VALUES ('DSV_OrderReleaseGen_Group_"+groupIndex+"','job-dsv-order-request-"+distributorId+"',false);";
+
+                String str2 = "INSERT INTO wbr_jobtype (jobtype,headers,message_domain,message_template,message_type,partner_org_id,partner_org_name,properties,tags) VALUES ('job-dsv-order-request-"+distributorId+"',null,'DEFAULTDOMAIN','DSV_TEMPLATE','DSV_GENERATION_ORDERREQUEST','"+distributorId+"','"+distributorName+"',null,null);";
+                System.out.println(str1);
+                System.out.println(str2);
+                count++;
+            }
+            myResultSet.close();
+
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+
+}
+
